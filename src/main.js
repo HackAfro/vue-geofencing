dcc3ad1ae7915ea4c0bbccae2059d8ac74994357
Richard Afro <richyafro@hotmail.com>
2019-03-25 07:15:05 +0100

    Add plugins

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VSocket from 'vue-socket.io';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
-Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'GOOGLE_MAPS_KEY',
+    libraries: 'geometry', // This is required if you use the Autocomplete
+  },
+});
+
+Vue.use(
+  new VSocket({
+    debug: true,
+    connection: 'http://localhost:4001',
+  })
+);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
